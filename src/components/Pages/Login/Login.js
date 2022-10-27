bd233ff7fbd779ea59d22130d6fcb7e510a684c5
ibrahimkhalilullahshafi <shafi.warx@gmail.com>
2022-10-27 19:59:53 +0600

    login and signup form sytem ready

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
 
 const Login = () => {
+    const [error, setError] = useState('');
+
+    const { signIn } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+
+    const submitHandler = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset();
+                setError('');
+                navigate('/')
+            })
+            .catch(error => {
+                console.error(error);
+                setError(error.message);
+            })
+
+    }
+
     return (
 
-        <Form>
+        <Form onSubmit={submitHandler}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" name="email" placeholder="Enter Email" required />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" name="password" placeholder="Enter Password" required />
             </Form.Group>
             <Button variant="primary" type="submit">
                 Submit
             </Button>
             <Form.Group><Form.Text className="text-danger">
-                We'll never share your email with anyone else.
+                {error}
             </Form.Text></Form.Group>
         </Form>
 
--- src/components/Pages/Login/SignUp.js
+++ src/components/Pages/Login/SignUp.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
 
 const SignUp = () => {
+    const { createUser } = useContext(AuthContext);
+
+    const [error, setError] = useState('');
+
+    const submitHandler = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photoURL = form.photoURL.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, photoURL, email, password);
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset();
+                setError('');
+            })
+            .catch(error => {
+                console.error(error);
+                setError(error.message);
+            })
+    }
+
     return (
-        <Form>
+        <Form onSubmit={submitHandler}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Full Name</Form.Label>
                 <Form.Control type="text" name="name" placeholder="Enter Full Name" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
+                <Form.Label>Your Photo</Form.Label>
                 <Form.Control type="text" name="photoURL" placeholder="Enter PhotoURL" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -27,6 +54,9 @@ const SignUp = () => {
             <Button variant="primary" type="submit">
                 Sign Up
             </Button>
+            <Form.Group><Form.Text className="text-danger">
+                {error}
+            </Form.Text></Form.Group>
         </Form>
     );
 };
